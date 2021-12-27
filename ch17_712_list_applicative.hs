38590107a97162825ddd195d30dc278bf4017b0d
Serhiy Yevtushenko <syevtushenko@rotary.org>
2021-12-27 18:57:07 +0200

    foo

--- ch17_712_list_applicative.hs
+++ ch17_712_list_applicative.hs
@@ -1,34 +1,33 @@
--- import           Control.Applicative
--- import           Data.Monoid
-import           Data.Semigroup
-import           Test.QuickCheck
-import           Test.QuickCheck.Checkers
-import           Test.QuickCheck.Classes
+import Data.Semigroup
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
-data List a = Nil | Cons a (List a) deriving (Eq, Show)
+data List a
+  = Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
 
 instance Monoid (List a) where
   mempty = Nil
-  mappend Nil         a   = a
-  mappend a           Nil = a
-  mappend (Cons x xs) ys  = Cons x $ mappend xs ys
+  mappend Nil a = a
+  mappend a Nil = a
+  mappend (Cons x xs) ys = Cons x $ mappend xs ys
 
 instance Semigroup (List a) where
   (<>) = mappend
-  -- Nil         <> a   = a
-  -- a           <> Nil = a
-  -- (Cons x xs) <> ys  = Cons x $ xs <> ys
 
 instance Functor List where
-  fmap _ Nil         = Nil
+  fmap _ Nil = Nil
   fmap f (Cons a la) = Cons (f a) (fmap f la)
 
 instance Applicative List where
-  pure a = undefined
-  (Cons f fl) <*> (Cons v vl) = Cons (f v) (fl <*> vl)
+  pure a = Cons a Nil
+  _ <*> Nil = Nil
+  Nil <*> _ = Nil
+  (Cons f fs) <*> values@(Cons v vs) = Cons (f v) (f <$> vs) <> (fs <*> values)
 
 -- pure id <*> v = v
-
 instance Eq a => EqProp (List a) where
   (=-=) = eq
 
@@ -39,6 +38,6 @@ instance Arbitrary a => Arbitrary (List a) where
 
 main :: IO ()
 main = do
-  quickBatch $ monoid (Cons 2 (Cons 3 Nil))
+  quickBatch $ applicative (undefined :: List (String, String, String))
+  -- quickBatch $ monoid (Cons "foo" (Cons "bar" Nil))
   -- quickBatch $ functor (undefined :: List (String, String, String))
-  -- quickBatch $ applicative (undefined :: List (String, String, String))
